f5ead03fcbc8e6558006779fd530fd10d0861ef1
Jvieyrah <keep@thinkeep-5260>
2022-05-25 18:00:18 -0300

    requisito 6 aprovado no cypress

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -25,26 +25,46 @@ class Game extends Component {
     const returnedQuestions = await getQuestions(localStorageToken);
     console.log(returnedQuestions.results);
     // verifica se o token é valido e desloga se nâo for.
-    const tree = 3;
-    if (returnedQuestions.response_code === tree) {
-      localStorage.storage.removeItem('token');
+    // const tree = 3;
+    if (returnedQuestions.results.length < 1) {
+      localStorage.removeItem('token');
       const { history } = this.props;
-      history.push('/');
-    } else {
-      // aloca array de perguntas no estado:
-      this.setState({
-        questionsArray: returnedQuestions.results,
-        isLoading: false,
-      });
+      return history.push('/');
     }
+    // aloca array de perguntas no estado:
+    this.setState({
+      questionsArray: returnedQuestions.results,
+    });
+    const { questionsArray, counter } = this.state;
+    const answers = [];
+    /* pega respostas erradas e coloca num array answers */
+    questionsArray[counter].incorrect_answers.forEach((wrongAnswer) => {
+      answers.push(wrongAnswer);
+    });
+    /* pega respostas certas e coloca num array answers */
+    answers.push(questionsArray[counter].correct_answer);
+    // cria botoes e os aloca na const buttons
+    const buttons = [];
+    answers.map((answer, index) => (
+      buttons.push(
+        <button
+          key={ index }
+          type="button"
+          data-testid={ answer === questionsArray[counter].correct_answer
+            ? 'correct-answer' : `wrong-answer-${index}` }
+        >
+          { answer }
+        </button>,
+      )));
+    this.setState({
+      answers: buttons,
+      isLoading: false,
+    });
   }
 
   render() {
     const { name, score, picture } = JSON.parse(localStorage.getItem('ranking'));
-    const { questionsArray, counter, isLoading, zerocinco } = this.state;
-    const answers = [];
-    // answers.sort((a, b) => b > a);
-    // const { category } = questionsArray[0];
+    const { questionsArray, counter, isLoading, zerocinco, answers } = this.state;
 
     return (
       <section>
@@ -70,25 +90,9 @@ class Game extends Component {
             <p data-testid="question-text">
               { questionsArray[counter].question }
             </p>
-            <div data-testif="answer-options">
-              {/* pega respostas erradas e coloca num array answers */}
-              { questionsArray[counter].incorrect_answers.forEach((wrongAnswer) => {
-                answers.push(wrongAnswer);
-              })}
-              {/* pega respostas certas e coloca num array answers */}
-              {answers.push(questionsArray[counter].correct_answer)}
-              {/* mistura as respostas de forma randomica */}
-              { (answers[Math.floor(Math.random() * answers.length)])}
-              { (answers.sort(() => Math.random() - zerocinco))
-                .map((answer, index) => (
-                  <button
-                    key={ index }
-                    type="button"
-                    data-testid={ answer === questionsArray[counter].correct_answer
-                      ? 'correct-answer' : `wrong-answer-${index}` }
-                  >
-                    { answer }
-                  </button>))}
+            <div data-testid="answer-options">
+              {(answers.sort(() => Math.random() - zerocinco))
+                .map((answer) => answer)}
             </div>
           </div>
         )}
