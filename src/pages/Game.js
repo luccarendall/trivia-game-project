8589f4b2559a6b152b1bf55d035f057a795f1a67
Thalys RD <tgbr66@gmail.com>
2022-05-25 12:48:05 -0300

    mudanças para fazer o pull

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,7 +2,20 @@ import React, { Component } from 'react';
 
 class Game extends Component {
   render() {
-    return (<section><h1>Game</h1></section>);
+    const { name, score, picture } = JSON.parse(localStorage.getItem('ranking'));
+    return (
+      <section>
+        <header>
+          <img
+            src={ picture }
+            alt="Avatar do jogador"
+            data-testid="header-profile-picture"
+          />
+          <div data-testid="header-player-name">{ name }</div>
+          <div data-testid="header-score">{ score }</div>
+        </header>
+      </section>
+    );
   }
 }
 
